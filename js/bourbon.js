6a6b8c907a400396fb2cc4bfefa079ac11637139
rhodocoder <lamc502@york.ac.uk>
2018-12-01 11:26:19 +0000

    Verification

---
+++ js/bourbon.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($){
+
+if (sessionStorage.getItem('advertOnce') !== 'true') {
+//sessionStorage.setItem('advertOnce','true');
+  $('.box').show();
+}else{
+  $('.box').hide();
+}
+ 
+$('#refresh-page').on('click',function(){
+$('.box').hide();
+sessionStorage.setItem('advertOnce','true');
+});
+  
+$('#reset-session').on('click',function(){
+$('.box').show();
+sessionStorage.setItem('advertOnce','');
+});
+ 
+});
