31abb32cdec4c232c8d83d991026016fef44a99d
rhodocoder <lamc502@york.ac.uk>
2018-12-02 22:05:33 +0000

    contents added website

--- contents.py
+++ contents.py
@@ -1,32 +1,32 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/index")
+@app.route("https://immense-ridge-70695.herokuapp.com/")
+@app.route("https://immense-ridge-70695.herokuapp.com/index.html")
 def index():
 	return render_template('index.html')
 
-@app.route("/superman")
+@app.route("https://immense-ridge-70695.herokuapp.com/superman.html")
 def superman():
     return render_template('superman.html')
 
-@app.route("/home")
+@app.route("https://immense-ridge-70695.herokuapp.com/home.html")
 def home():
     return render_template('home.html')
 
-@app.route("/about")
+@app.route("https://immense-ridge-70695.herokuapp.com/about.html")
 def about():
     return render_template('about.html')
 
-@app.route("/recipesideas")
+@app.route("https://immense-ridge-70695.herokuapp.com/recipesideas.html")
 def recipesideas():
     return render_template('recipesideas.html')
 
-@app.route("/instagramsuggestions")
+@app.route("https://immense-ridge-70695.herokuapp.com/instagramsuggestions.html")
 def instagramsuggestions():
     return render_template('instagramsuggestions.html')
 
-@app.route("/contact")
+@app.route("https://immense-ridge-70695.herokuapp.com/contact.html")
 def contact():
     return render_template('contact.html')
 
